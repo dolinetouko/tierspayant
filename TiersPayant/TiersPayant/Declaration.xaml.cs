c0cad8b9cded0e94edc1f56bc7769d8dcfbc6de3
doline touko <touko.doline@gmail.com>
2019-08-18 02:55:04 +0200

    projet init

---
+++ TiersPayant/TiersPayant/Declaration.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TiersPayant
+{
+    /// <summary>
+    /// Logique d'interaction pour Declaration.xaml
+    /// </summary>
+    public partial class Declaration : Window
+    {
+        public String souscripteur;
+        public String nom;
+        public String matricule;
+        public String centre;
+        public int police;
+        public String echeance;
+        public String prestation;
+        
+
+        public Declaration()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TiersPayant/TiersPayant/PageAccueil.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TiersPayant
+{
+    /// <summary>
+    /// Logique d'interaction pour PageAccueil.xaml
+    /// </summary>
+    public partial class PageAccueil : Window
+    {
+        public PageAccueil()
+        {
+            InitializeComponent();
+        }
+
+       /* private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed; 
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }*/
+        /*private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }*/
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Hide();
+        }
+        private void listViewMenu_SelectionChange(object sender, RoutedEventArgs e)
+        {
+            int index = listViewMenu.SelectedIndex;
+           // MoveCursorMenu(index);
+
+            switch(index){
+
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControl2());
+                    break;
+                case 1:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControl1());
+                    break;
+                case 2:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControl4());
+                    break;
+                case 3:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControl3());
+                    break;
+                default:
+                    break;
+        }
+
+        }
+        /*private void MoveCursorMenu(int index)
+        {
+            transitionigContentSlice.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, 0, 0, (224 + (60 * index)));
+
+        }*/
+
+        
+    }
+}
